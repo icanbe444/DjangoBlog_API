1ae0ac4ffd49f93ba0347ce9e5a2ce2e254325d5
icanbe444 <icanbe444@gmail.com>
2022-08-02 15:56:54 +0100

    Added the appropriate comments

--- blog/api/permissions.py
+++ blog/api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+#permission is set to determine the ownership of a post
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
